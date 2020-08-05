1da966e0523410b4c178cadd0524e237e429b544
Cristian Mateos <mateos.c19@gmail.com>
2020-08-05 15:26:50 +0200

    Setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express = require('express');
+import express, {Application} from 'express';
 
-const app: express.Application = express();
+const app: Application = express();
 
 app.get('/', function (request, response) {
   response.send('Hello world');
