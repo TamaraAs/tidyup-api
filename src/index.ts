b2ccc46ef070fb5b4407344e38af7da9759c93c5
Cristian Mateos <mateos.c19@gmail.com>
2020-08-05 15:29:38 +0200

    Setup

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import express, {Application} from 'express';
 
 const app: Application = express();
 
-app.get('/', function (request, response) {
-  response.send('Hello world');
+app.get('/', (request, response) => {
+  response.send('Hello world!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('App is listening on port 3000');
 });
