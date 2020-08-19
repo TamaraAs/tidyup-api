eef1539bc2deb042b9f0f5a85080dd966eba10bc
Cristian Mateos <mateos.c19@gmail.com>
2020-08-19 17:58:18 +0200

    Add new verbs for resource box

--- src/routes/box-route.ts
+++ src/routes/box-route.ts
@@ -4,7 +4,10 @@ import * as boxController from '../controllers/box-controller';
 export default (server: Application): void => {
   const router = Router();
 
-  router.get('/boxes', boxController.search);
+  router.get('', boxController.search);
+  router.post('', boxController.create);
+  router.get('/:id', boxController.findById);
+  router.delete('/:id', boxController.remove);
 
-  server.use('', router);
+  server.use('/boxes', router);
 };
--- src/services/box-service.ts
+++ src/services/box-service.ts
@@ -1,9 +1,21 @@
 import { Box } from '../interfaces/box';
 import boxRepository from '../models/box-model';
 
-export async function search(): Promise<Box[]> {
-  const searchedBoxes = await boxRepository.find().lean().exec();
+const search = async (): Promise<Box[]> => {
+  const searchedBoxes = await boxRepository.find().exec();
   return searchedBoxes.map((element) => {
     return { id: element._id };
   });
-}
+};
+
+const create = async (): Promise<Box> => {
+  const findedBox = await boxRepository.create({});
+  return { id: findedBox?._id };
+};
+
+const findById = async (id: string): Promise<Box> => {
+  const findedBox = await boxRepository.findById(id).exec();
+  return { id: findedBox?._id };
+};
+
+export { search, create, findById };
