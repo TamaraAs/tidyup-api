68240411f358a5d7c2ba6c2a3f97fc1043e1c50f
Cristian Mateos <mateos.c19@gmail.com>
2020-09-16 20:58:13 +0200

    Add controller tests

--- test/TestUtils.ts
+++ test/TestUtils.ts
@@ -1,13 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { mock, MockProxy } from 'jest-mock-extended';
-
-export function createRequest(): Request {
-  return {} as Request;
-}
 
 export function createMockResponse(): MockProxy<Response> {
   const mockResponse = mock<Response>();
   mockResponse.json.mockReturnThis();
   mockResponse.status.mockReturnThis();
   return mockResponse;
+}
+
+export class RequestBuilder {
+  private body: Record<string, unknown>;
+  private headers: Record<string, unknown>;
+  private params: Record<string, string>;
+
+  withBody(body: Record<string, unknown>): RequestBuilder {
+    this.body = body;
+    return this;
+  }
+
+  withHeaders(headers: Record<string, unknown>): RequestBuilder {
+    this.headers = headers;
+    return this;
+  }
+
+  withParams(params: Record<string, string>): RequestBuilder {
+    this.params = params;
+    return this;
+  }
+
+  build(): Request {
+    return {
+      body: this.body,
+      headers: this.headers,
+      params: this.params
+    } as Request;
+  }
 }
--- test/controllers/BoxController.spec.ts
+++ test/controllers/BoxController.spec.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { NextFunction, Response } from 'express';
 import { mock, MockProxy } from 'jest-mock-extended';
-import { createRequest, createMockResponse } from '../TestUtils';
+import { createMockResponse, RequestBuilder } from '../TestUtils';
 import { BoxService } from '../../src/services/BoxService';
 import { BoxController } from '../../src/controllers/BoxController';
 import { Box } from '../../src/models/Box';
@@ -24,7 +24,8 @@ describe('BoxController unit tests', () => {
       const expectedItems: Box[] = [];
       mockService.getBoxes.mockResolvedValue(expectedItems);
 
-      await boxController.getAll(createRequest(), mockResponse, mock());
+      const request = new RequestBuilder().build();
+      await boxController.getAll(request, mockResponse, mock());
 
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledWith(expectedItems);
@@ -34,7 +35,108 @@ describe('BoxController unit tests', () => {
       const error = new Error('dummy error');
       mockService.getBoxes.mockRejectedValue(error);
 
-      await boxController.getAll(createRequest(), mockResponse, mockNextFN);
+      const request = new RequestBuilder().build();
+      await boxController.getAll(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.status).not.toHaveBeenCalled();
+      expect(mockResponse.json).not.toHaveBeenCalled();
+      expect(mockNextFN).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('create method', () => {
+    it('create on success insert return item created', async () => {
+      const expectedBox: Box = new Box('dummy', 'dummy name');
+      mockService.create.mockResolvedValue(expectedBox);
+
+      const request = new RequestBuilder().withBody({ item: 'value' }).build();
+      await boxController.create(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(201);
+      expect(mockResponse.json).toHaveBeenCalledWith(expectedBox);
+    });
+
+    it('create on fail insert call next with error', async () => {
+      const error = new Error('dummy error');
+      mockService.create.mockRejectedValue(error);
+
+      const request = new RequestBuilder().withBody({ item: 'value' }).build();
+      await boxController.create(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.status).not.toHaveBeenCalled();
+      expect(mockResponse.json).not.toHaveBeenCalled();
+      expect(mockNextFN).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('getById method', () => {
+    it('getById with content-type application/json, on success search, return item', async () => {
+      const expectedBox: Box = new Box('dummy', 'dummy name');
+      mockService.findById.mockResolvedValue(expectedBox);
+
+      const request = new RequestBuilder()
+        .withHeaders({
+          'content-type': 'application/json'
+        })
+        .withParams({
+          id: 'id-dummy'
+        })
+        .build();
+      await boxController.getById(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith(expectedBox);
+      expect(mockResponse.end).not.toHaveBeenCalled();
+    });
+
+    it('getById with content-type image/png, on success search, return item stream', async () => {
+      const expectedBox: Box = new Box('dummy', 'dummy name');
+      mockService.findById.mockResolvedValue(expectedBox);
+
+      const request = new RequestBuilder()
+        .withHeaders({
+          'content-type': 'image/png'
+        })
+        .withParams({
+          id: 'id-dummy'
+        })
+        .build();
+      await boxController.getById(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.end).toHaveBeenCalled(); // TODO: When refactor qrcode improve test.
+      expect(mockResponse.status).not.toHaveBeenCalled();
+      expect(mockResponse.json).not.toHaveBeenCalled();
+    });
+
+    it('getById with no register content-type, return 415 http error code', async () => {
+      const request = new RequestBuilder()
+        .withHeaders({
+          'content-type': 'dummy'
+        })
+        .withParams({
+          id: 'dummy-id'
+        })
+        .build();
+      await boxController.getById(request, mockResponse, mockNextFN);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(415);
+      expect(mockResponse.send).toHaveBeenCalled();
+      expect(mockResponse.json).not.toHaveBeenCalled();
+    });
+
+    it('getById with valid content-type, on failed search, call next with error', async () => {
+      const error = new Error('dummy error');
+      mockService.findById.mockRejectedValue(error);
+
+      const request = new RequestBuilder()
+        .withHeaders({
+          'content-type': 'application/json'
+        })
+        .withParams({
+          id: 'dummy-id'
+        })
+        .build();
+      await boxController.getById(request, mockResponse, mockNextFN);
 
       expect(mockResponse.status).not.toHaveBeenCalled();
       expect(mockResponse.json).not.toHaveBeenCalled();
